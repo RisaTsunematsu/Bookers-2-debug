bda9a8adfe75e9a3e22f0d6aff9d6abd416121d3
EC2 Default User <ec2-user@ip-172-31-21-177.ap-northeast-1.compute.internal>
2023-02-28 13:17:10 +0000

    フォローフォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,16 @@ Rails.application.routes.draw do
  get "/home/about" => "homes#about", as: "about"
  
 
-resources :users
-resources :books do
- resource :favorites, only: [:create, :destroy]
+ resources :users
+ resources :books do
+  resource :favorites, only: [:create, :destroy]
  resources :book_comments, only: [:create, :destroy]
-end
+ end
+
+ resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
 
